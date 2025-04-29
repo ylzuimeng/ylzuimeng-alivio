8f81c4aa35b2ba1e713c8dcd07c0e77d436adb51
杨磊 <ylzuimeng@gmail.com>
2025-04-29 12:55:03 +0800

    回复到以前的版本

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from ui.main_window import MainWindow
-from ui.setting_panel import SettingPanel
-from config.config_manager import ConfigManager
 
 def main():
     app = MainWindow()
     app.run()
-
+ 
 if __name__ == "__main__":
     main() 
